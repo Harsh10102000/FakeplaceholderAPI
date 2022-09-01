31097f4140ec6e565ca65e073b92795e9e1eec9e
Harsh Trivedi <Harsh10102000@gmail.com>
2022-09-01 10:30:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+
+import React , { Component  } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import FormControl from './components/FormControl';
+
+class App extends Component() {
+
+ constructor(props){
+  super(props)
+  this.state = {
+    users : [],   //here we setting up default value
+    isLoading : false ,
+    isError : false  // If there is any error by default its false
+  }
+ }
+
+   // Async Function to get a request from an API
+   async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    // console.log(response)
+    if (response.ok) { //Ok is property if it true then if condition follow otherwise else
+      const users = await response.json()
+      //console.log(users)
+      this.setState({ users, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
+    }
+  }
+  
+  renderTableHeader = () => {
+    return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr.toUpperCase()}</th>)
+  }
+
+  renderTableRows = () => {
+    return this.state.users.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.username}</td>
+          <td>{user.email}</td>
+          <td>{`${user.address.street}, ${user.address.city}`}</td>
+          <td>{user.phone}</td>
+          <td>{user.website}</td>
+          <td>{user.company.name}</td>
+        </tr>
+      )
+    })
+  }
+
+  render(){
+    const { users, isLoading, isError } = this.state
+ 
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
+ 
+    if (isError) {
+      return <div>Error</div>
+    }
+ 
+    return users.length > 0
+      ? (
+        <table>
+          <thead>
+            <tr>
+              {this.renderTableHeader()}
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableRows()}
+          </tbody>
+        </table>
+      ) : (
+        <div>
+          No users.
+      </div>
+      )
+  }
+
 }
+
 
 export default App;
---
+++ src/components/FormControl.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react'
+
+class FormControl extends Component{
+
+  render(){
+    return (
+        <div>Hello world</div>
+    )
+  }
+
+}
+
+export default FormControl
